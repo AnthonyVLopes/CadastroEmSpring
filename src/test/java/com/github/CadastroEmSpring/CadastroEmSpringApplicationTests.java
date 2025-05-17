16ae54471acbbb2ec6be60203c0abfc86cb194b1
AnthonyVLopes <anthony.2219v@gmail.com>
2025-05-17 14:57:24 -0300

    first commit

---
+++ src/test/java/com/github/CadastroEmSpring/CadastroEmSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.CadastroEmSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEmSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
