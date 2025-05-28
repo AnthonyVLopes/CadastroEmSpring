d0e6462aaa0ebf8d97740ab7c18164794390639f
Anthony Lopes <125279600+AnthonyVLopes@users.noreply.github.com>
2025-05-28 17:26:56 -0300

    Criação do Model

---
+++ src/main/java/com/github/CadastroEmSpring/controller/usuarioController.java
@@ -0,0 +1,15 @@
+package com.github.CadastroEmSpring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class usuarioController {
+
+    @GetMapping("/home")
+    public String homepage(){
+        return "Seja bem vindo";
+    }
+}
---
+++ src/main/java/com/github/CadastroEmSpring/model/usuarioModel.java
@@ -0,0 +1,47 @@
+package com.github.CadastroEmSpring.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_usuario")
+public class usuarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public usuarioModel() {
+    }
+
+    public usuarioModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
